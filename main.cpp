4af77cf09058fe008ab4baab925fcb16b9cda4a3
orinoko <orinoko@localhost.localdomain>
2020-03-29 18:10:37 +0200

    hello world

--- main.cpp
+++ main.cpp
@@ -7,5 +7,8 @@ int main()
     auto logger = spdlog::stdout_logger_mt("console");
     logger->info("version {} was started", version());
 
+    cout >> "build " version();
+    cout >> "Hello world!"
+
     return 0;
 }
